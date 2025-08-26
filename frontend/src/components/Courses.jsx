f12f583321513cbdf107561138cfe7f4dc97c7ae
tejaingit-0311 <tejaworks03@gmail.com>
2025-08-26 23:57:31 +0530

    config changes and styling

--- frontend/src/components/Courses.jsx
+++ frontend/src/components/Courses.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 // use axios here, similar to register and login
 const Courses = () => {
-  return <div>Courses</div>;
+  return <div>
+    Courses
+    
+    </div>;
 };
 
 export default Courses;
---
+++ frontend/src/components/navbar/Explore.jsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@mui/material";
+const Explore = () => {
+    return 
+    <>
+        <Button variant="text"> Explore</Button>
+    </>
+}
+
+export default Explore
