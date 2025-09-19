72b8718894a761a353433e9e5aa27023d945ebce
tejaingit-0311 <tejaworks03@gmail.com>
2025-09-19 12:38:57 +0530

    user and admin changes

--- backend/middleware/adminMiddlware.js
+++ backend/middleware/adminMiddlware.js
@@ -6,33 +6,37 @@ function adminMiddleware(req, res, next) {
   // console.log(req);
   //extract token from header -> verify token with SIGN ->delegate the req
   try {
-    let authToken = req.headers["authorization"];
-    if (!authToken) {
-      res.status(401).send({
-        success: false,
-        error: { code: "TOKEN_NOT_FOUND", message: "Token was not there" },
-      });
-      return;
-    }
+    const authToken = req.headers["authorization"].substring(7);
+
     console.log(authToken);
-    const token = authToken.substring(7, authToken.length);
-    console.log(token);
     // console.log(token);
 
     try {
-      const adminUser = jwt.verify(token, process.env.JWT_SECRET);
-      req.adminUserId = adminUser.id;
+      const adminUser = jwt.verify(authToken, process.env.JWT_SECRET);
+      const adminUserData = 
+      {
+        adminUserId :adminUser.id,
+        adminUserEmail : adminUser.email
+      }
+      req.adminUser = adminUserData;
       // console.log(req.adminUserId);
       next();
-    } catch (error) {
+    } // for invalid token: send a generic response , and log actual error in different file:S
+    catch (error) {
       return res.status(401).json({
         success: false,
-        error: { code: "INVALID_TOKEN", message: "Not a Correct Token" },
+        error: { code: "UNAUTHORIZED", message: "Cannot Access Content" },
       });
     }
   } catch (error) {
     // res.status(500).json();
-    // res.status(500).json({success:false , error: {code: "INTERNAL_SERVER_ERROR", message: "Something Went Wrong"}});
+    res.status(500).json({
+      success: false,
+      error: {
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Something Went Wrong",
+      },
+    });
   }
 }
 
